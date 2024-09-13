c72d5c2fe088e771fa2150d03ba8c7d99cef8415
christian_burger <christian_burger@yahoo.com>
2024-09-13 13:03:39 +0000

    Adding gazebo launch file and correlated changes.

--- launch/gazebo_sim.launch.py
+++ launch/gazebo_sim.launch.py
@@ -1,6 +1,8 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import Command, LaunchConfiguration
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.parameter_descriptions import ParameterValue
 from ament_index_python.packages import get_package_share_directory
 import os
@@ -8,13 +10,27 @@ import os
 def generate_launch_description():
     pkg_share = get_package_share_directory('shelfbot')
     urdf_file_path = os.path.join(pkg_share, 'urdf', 'shelfbot.urdf.xacro')
-    default_rviz_config_path = os.path.expanduser('~/.rviz2/shelfbot.rviz')  # Expand ~ here
-
-    rviz_config_file = LaunchConfiguration('rviz_config', default=default_rviz_config_path)
+    world_file_path = os.path.join(pkg_share, 'worlds', 'shelfbot_world.world')
 
     robot_description = ParameterValue(Command(['xacro ', urdf_file_path]), value_type=str)
 
     return LaunchDescription([
+        # Launch Gazebo
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+            launch_arguments={'world': world_file_path}.items()
+        ),
+
+        # Spawn robot in Gazebo
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=['-topic', 'robot_description', '-entity', 'shelfbot'],
+            output='screen'
+        ),
+
+        # Publish robot state
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
@@ -22,18 +38,13 @@ def generate_launch_description():
             output='screen',
             parameters=[{'robot_description': robot_description}],
         ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_transform_publisher',
-            output='screen',
-            arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'base_footprint'],
-        ),
+
+        # Launch RViz2
         Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
-            output='screen',
-            arguments=['-d', rviz_config_file],
+            arguments=['-d', os.path.join(pkg_share, 'rviz2', 'shelfbot_config.rviz')],
+            output='screen'
         ),
     ])
