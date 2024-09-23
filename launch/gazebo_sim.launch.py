b76ebec6e570de14cf30ef0ae3a35009e79d08c6
christian_burger <christian_burger@yahoo.com>
2024-09-23 22:14:44 +0000

    Fixing the launch file.

--- launch/gazebo_sim.launch.py
+++ launch/gazebo_sim.launch.py
@@ -1,75 +1,111 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.actions import ExecuteProcess, RegisterEventHandler
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, ExecuteProcess
+from launch.conditions import IfCondition
 from launch.event_handlers import OnProcessExit
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, EnvironmentVariable
+from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value="true",
+            description="Use simulation (Gazebo) clock if true",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gui",
+            default_value="true",
+            description="Start Gazebo GUI if true",
+        )
+    )
+
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    gui = LaunchConfiguration("gui")
+
     pkg_share = FindPackageShare(package='shelfbot').find('shelfbot')
     urdf_file_path = PathJoinSubstitution([pkg_share, 'urdf', 'shelfbot.urdf.xacro'])
     
-    world_file_path = '~/.world/shelfbot_world.world'
-    
-    robot_description_content = ParameterValue(Command([
-        FindExecutable(name='xacro'), ' ', urdf_file_path
-    ]), value_type=str)
-
-    robot_description = {'robot_description': robot_description_content}
+    robot_description_content = Command(
+        [
+            FindExecutable(name="xacro"),
+            " ",
+            urdf_file_path,
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
 
     controller_config = PathJoinSubstitution([pkg_share, 'config', 'four_wheel_drive_controller.yaml'])
-
-    return LaunchDescription([
-        ExecuteProcess(
-            cmd=['gazebo', '--verbose', world_file_path, '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
-            output='screen'
-        ),
-
-        Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
-            arguments=['-topic', 'robot_description', '-entity', 'shelfbot'],
-            output='screen'
-        ),
-
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[robot_description],
-        ),
-
-        Node(
-            package='controller_manager',
-            executable='ros2_control_node',
-            parameters=[robot_description, controller_config],
-            output='screen',
-        ),
-
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=Node(
-                    package='controller_manager',
-                    executable='ros2_control_node',
-                    parameters=[robot_description, controller_config],
-                    output='screen',
-                ),
-                on_exit=[
-                    Node(
-                        package='controller_manager',
-                        executable='spawner',
-                        arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager'],
-                        output='screen',
-                    ),
-                    Node(
-                        package='controller_manager',
-                        executable='spawner',
-                        arguments=['four_wheel_drive_controller', '--controller-manager', '/controller_manager'],
-                        output='screen',
-                    ),
-                ],
-            )
-        ),
+    
+    world_file_path = PathJoinSubstitution([
+        EnvironmentVariable('HOME'),
+        '.world',
+        'shelfbot_world.world'
     ])
+
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=world_file_path,
+        description='Path to the world file'
+    )
+
+    gazebo_node = ExecuteProcess(
+        cmd=['gazebo', '--verbose', LaunchConfiguration('world'), '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
+        output='screen',
+        condition=IfCondition(gui)
+    )
+
+    spawn_entity_node = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'shelfbot'],
+        output='screen'
+    )
+
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, controller_config],
+        output="both",
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["four_wheel_drive_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[robot_controller_spawner],
+        )
+    )
+
+    nodes = [
+        world_arg,
+        gazebo_node,
+        spawn_entity_node,
+        robot_state_pub_node,
+        control_node,
+        joint_state_broadcaster_spawner,
+        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
+    ]
+
+    return LaunchDescription(declared_arguments + nodes)
