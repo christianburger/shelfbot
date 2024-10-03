b11e81ba695f1b039532377d9f9a57d148291169
christian_burger <christian_burger@yahoo.com>
2024-10-03 16:13:54 +0000

    Adding troubleshooting code, refactoring.

--- include/four_wheel_drive_controller.hpp
+++ include/four_wheel_drive_controller.hpp
@@ -8,29 +8,23 @@
 #include "controller_interface/controller_interface.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/state.hpp"
+#include "shelfbot_utils.hpp"
 #include "std_msgs/msg/float64.hpp"
 #include "std_msgs/msg/float64_multi_array.hpp"
 
 namespace shelfbot {
 
-class FourWheelDriveController
-    : public controller_interface::ControllerInterface {
+class FourWheelDriveController : public controller_interface::ControllerInterface {
  public:
   FourWheelDriveController();
 
-  controller_interface::InterfaceConfiguration command_interface_configuration()
-      const override;
-  controller_interface::InterfaceConfiguration state_interface_configuration()
-      const override;
-  controller_interface::return_type update(
-      const rclcpp::Time& time, const rclcpp::Duration& period) override;
+  controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+  controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+  controller_interface::return_type update(const rclcpp::Time& time, const rclcpp::Duration& period) override;
   controller_interface::CallbackReturn on_init() override;
-  controller_interface::CallbackReturn on_configure(
-      const rclcpp_lifecycle::State& previous_state) override;
-  controller_interface::CallbackReturn on_activate(
-      const rclcpp_lifecycle::State& previous_state) override;
-  controller_interface::CallbackReturn on_deactivate(
-      const rclcpp_lifecycle::State& previous_state) override;
+  controller_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
+  controller_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
+  controller_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
 
  private:
   std::vector<std::string> joint_names_;
@@ -38,13 +32,12 @@ class FourWheelDriveController
   std::map<std::string, double> axis_commands_;
 
   rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr cmd_sub_;
-  std::map<std::string, rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr>
-      joint_state_pubs_;
+  std::map<std::string, rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr> joint_state_pubs_;
 
   void cmd_callback(const std_msgs::msg::Float64MultiArray::SharedPtr msg);
   void publish_joint_states();
 };
 
-}
+} 
 
 #endif
